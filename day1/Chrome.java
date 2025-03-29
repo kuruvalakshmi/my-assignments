e874f45ffbe7c94e32441c6e65bda248b30a6e8c
kuruvalakshmi <kuruvalakshmi30596@gmail.com>
2025-03-30 00:11:12 +0530

    Add files via upload
    Assignment

---
+++ day1/Chrome.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// float
+		float version=91.0f;
+		System.out.println(version);
+		
+		//string
+		String developer="Google";
+		System.out.println(developer);
+		
+		//boolean
+	    boolean isBeta=false;
+	    System.out.println(isBeta);
+	    
+	  //int
+	    int releaseYear=2008;
+	    System.out.println(releaseYear);
+	    
+	  //char
+	    char shortCut='C';
+	    System.out.println(shortCut);
+
+	}
+
+}
